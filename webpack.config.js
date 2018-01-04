22b4641ee7278af34f0cf8cd42b760512b4f643e
dianavashti <diana.vashti@gmail.com>
2018-01-04 15:47:26 -0800

    Adds webpack build files to gitignore and updates build path

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack')
 const path = require( 'path' )
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const jsonLoader = require('json-loader');
-const BUILD_DIR = path.resolve(__dirname, 'public')
+const BUILD_DIR = path.resolve(__dirname, 'public/build/')
 const APP_DIR = path.resolve(__dirname, 'client')
 
 const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
